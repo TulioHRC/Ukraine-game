2e4f89d39cef6887616df005a1290534be52bd97
Jocker00 <grove1411@gmail.com>
2022-03-07 20:23:10 -0300

    0.2.1v - Others features

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <Game 
-        image1={'https://img2.gratispng.com/20180422/auw/kisspng-vladimir-putin-president-of-russia-security-counci-celebrity-5add0c13d6f1a7.5855779815244359878804.jpg'} 
+        image1={'putin.png'} 
         image2={'https://static.poder360.com.br/2022/02/azov-fev-2022-848x477.jpeg'} 
         image3={'Ukranian.png'} 
-        image4={'https://poplembrancinhas.com.br/wp-content/uploads/2020/09/elefante-desenho-lindo-1.jpg'} 
+        image4={'elefante.png'} 
       />
     </div>
   );
